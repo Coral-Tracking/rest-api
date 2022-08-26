efb9e68fa9589b042bd07b28c7d4383b11f99278
rahmannurhidayat022 <rahmannurhidayat022@gmail.com>
2022-08-26 23:39:35 +0700

    Create crud area

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mainController = require("../controllers/mainController");
 const router = express.Router();
 
-router.get("/area", mainController.getArea);
+router.get("/area/:areaId", mainController.getArea);
 router.get("/areas", mainController.getAreas);
 router.post("/area", mainController.createArea);
-router.put("/area", mainController.updateArea);
-router.delete("/area", mainController.removeArea);
+router.put("/area/:areaId", mainController.updateArea);
+router.delete("/area/:areaId", mainController.removeArea);
 
 module.exports = router;
